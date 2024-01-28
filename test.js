91635dce47ed1e7047c8d22d8bbaa30f4e4f6bfe
hung-o7 <145742161+hung-o7@users.noreply.github.com>
2024-01-28 14:12:29 -0600

    Add files via upload

--- test.js
+++ test.js
@@ -1,21 +1,22 @@
-function test1(){
-var client_id = '0bcfe0e23dbe4f00bd2d338121bcbcd8';
-var redirect_uri = 'https://hung-o7.github.io/testingAPI/';
+(function test1(){
 
-var app = express();
-
-app.get('/login', function(req, res) {
-
-  var state = generateRandomString(16);
-  var scope = 'user-read-private user-read-email';
-
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
-});
-}
+    var client_id = '0bcfe0e23dbe4f00bd2d338121bcbcd8';
+    var redirect_uri = 'http://localhost:8888/callback';
+    
+    var app = express();
+    
+    app.get('/login', function(req, res) {
+    
+      var state = generateRandomString(16);
+      var scope = 'user-read-private user-read-email';
+    
+      res.redirect('https://accounts.spotify.com/authorize?' +
+        querystring.stringify({
+          response_type: 'code',
+          client_id: client_id,
+          scope: scope,
+          redirect_uri: redirect_uri,
+          state: state
+        }));
+    });
+})();
